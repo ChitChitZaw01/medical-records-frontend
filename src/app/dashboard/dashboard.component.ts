d298631f34b404fe0b768e21bc0ee9d8b6480274
ChitChitZaw01 <chitchitzaw.ccz16@gmail.com>
2025-03-19 19:33:39 +0700

    updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+import { CommonModule } from '@angular/common';
+
+interface Records {
+  patient_id: number;
+  doctor_id: number;
+  diagnosis: string;
+  treatment: string;
+  created_at: Date;
+}
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  private apiUrl = `${environment.apiUrl}`;
+  records: Records[] = [];  
+
+
+  constructor(private http: HttpClient) {}
+
   message = 'Welcome to your Dashboard!';
+
+  
+  ngOnInit() {
+    // Fetch players data on component initialization
+    this.getRecords().subscribe(
+      (data) => {
+        this.records = data;  // Store the players data
+      },
+      (error) => {
+        console.error('Error fetching records data:', error);
+      }
+    );
+  }
+
+  // Method to get records data from the API
+  getRecords(): Observable<Records[]> {
+    return this.http.get<Records[]>(this.apiUrl + '/medical-records');
+  }
 }
